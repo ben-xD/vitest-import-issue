1a3213daef7f3e8c87281376b7cd4f9d71080c84
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-01-17 14:24:15 +0000

    docs: improve comments and add link to open in stackblitz

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Reproducible example for broken protobuf imports
 
+This issue started showing in vitest 0.25.8. This was not a problem prior to that.
+
 ## Usage
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/ben-xD/vitest-import-issue)
 
 - Install pnpm: run `npm install --global pnpm`
 - Install dependencies: run `pnpm i`
--- src/example.spec.ts
+++ src/example.spec.ts
@@ -1,9 +1,9 @@
-// This issue started showing in vitest 0.25.8. This was not a problem prior to that.
-
-// Correct existing usage, doesn't work with Vitest:
+// My existing import, which doesn't work with Vitest:
 import {Any} from "google-protobuf/google/protobuf/any_pb";
-// Works with Vitest, but not in the application:
+
+// This import works with Vitest and application
 import AnyDefault from "google-protobuf/google/protobuf/any_pb";
+
 describe('Example', () => {
     it('instantiates', () => {
         console.log({Any}) // Undefined when running with vitest, but defined in `index.ts`
